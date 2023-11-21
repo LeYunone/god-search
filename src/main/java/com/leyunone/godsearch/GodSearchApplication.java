a8b20090bea1d381a1e7af118486a17cf7f1176d
LeYunone <365627310@qq.com>
2023-11-21 23:46:40 +0800

    Initial commit

---
+++ src/main/java/com/leyunone/godsearch/GodSearchApplication.java
@@ -0,0 +1,13 @@
+package com.leyunone.godsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GodSearchApplication.class, args);
+    }
+
+}
