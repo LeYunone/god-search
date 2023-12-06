9154aca590fa5971b6dcf9ab3b85283038fa1e94
LeYunone <365627310@qq.com>
2023-12-06 23:39:36 +0800

    [init]manager

---
+++ god-search-core/src/main/java/com/leyunone/godsearch/core/manage/base/TokenManager.java
@@ -0,0 +1,11 @@
+package com.leyunone.godsearch.core.manage.base;
+
+/**
+ * :)
+ * token 管理
+ * @author LeYunone
+ * @email 365627310@qq.com
+ * @date 2023/12/6
+ */
+public interface TokenManager {
+}
---
+++ god-search-core/src/main/java/com/leyunone/godsearch/core/manage/token/WebTokenManager.java
@@ -0,0 +1,13 @@
+package com.leyunone.godsearch.core.manage.token;
+
+import com.leyunone.godsearch.core.manage.base.TokenManager;
+
+/**
+ * :)
+ *
+ * @author LeYunone
+ * @email 365627310@qq.com
+ * @date 2023/12/6
+ */
+public class WebTokenManager implements TokenManager {
+}
