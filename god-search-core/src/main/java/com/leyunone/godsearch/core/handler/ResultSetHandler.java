ed51cc005d1b7e0248dcec3d4827f1e065ba7b0e
LeYunone <365627310@qq.com>
2023-12-09 23:17:43 +0800

    [init]manager

---
+++ god-search-core/src/main/java/com/leyunone/godsearch/core/handler/ResultSetHandler.java
@@ -0,0 +1,13 @@
+package com.leyunone.godsearch.core.handler;
+
+/**
+ * :)
+ *
+ * @author LeYunone
+ * @email 365627310@qq.com
+ * @date 2023/12/9
+ */
+public interface ResultSetHandler<T> {
+
+    String handler(T t);
+}
---
+++ god-search-core/src/main/java/com/leyunone/godsearch/core/social/handler/InfoCombinationHandler.java
@@ -0,0 +1,29 @@
+package com.leyunone.godsearch.core.social.handler;
+
+import com.leyunone.godsearch.core.bean.data.SocialUserInfo;
+import com.leyunone.godsearch.core.handler.ResultSetHandler;
+
+/**
+ * :)
+ *  社工部分 信息组合处理
+ * @author LeYunone
+ * @email 365627310@qq.com
+ * @date 2023/12/9
+ */
+public class InfoCombinationHandler implements ResultSetHandler<SocialUserInfo> {
+
+
+    /**
+     * 本处理器做以下操作：
+     * 1、组合排列已知的基本信息
+     * 2、符合人文设置的组合
+     * 3、
+     * @param socialUserInfo
+     * @return
+     */
+    @Override
+    public String handler(SocialUserInfo socialUserInfo) {
+
+        return null;
+    }
+}
